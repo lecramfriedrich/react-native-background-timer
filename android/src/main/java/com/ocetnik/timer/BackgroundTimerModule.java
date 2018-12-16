8e9fd7429e6ab4dc697567ebd48129266543f2f2
Marcel Friedrich <admin@marcel-friedrich.com>
2018-12-16 21:08:39 +0100

    fix wakelock

--- android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
+++ android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
@@ -7,6 +7,8 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 import java.lang.Runnable;
 
@@ -15,10 +17,12 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
     private Handler handler;
     private ReactContext reactContext;
     private Runnable runnable;
+	private WakeLock mWakeLock;
 
     public BackgroundTimerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+		this.mWakeLock = ((PowerManager) getReactApplicationContext().getSystemService("power")).newWakeLock(1, "TimerServiceLock");
     }
 
     @Override
@@ -28,6 +32,7 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void start() {
+		this.mWakeLock.acquire();
         handler = new Handler();
         runnable = new Runnable() {
             @Override
@@ -41,6 +46,9 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void stop() {
+		if (this.mWakeLock.isHeld()) {
+            this.mWakeLock.release();
+        }
         // avoid null pointer exceptio when stop is called without start
         if (handler != null) handler.removeCallbacks(runnable);
     }
